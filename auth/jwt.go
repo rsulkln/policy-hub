bb738a5e00e54a2b813e7807efc6747604434129
rasol kalantari <imrkalantari@gmail.com>
2025-08-11 10:29:50 +0330

    add JWT token generation and identification

---
+++ auth/jwt.go
@@ -0,0 +1,28 @@
+package jwti
+
+import (
+	"fmt"
+	"github.com/golang-jwt/jwt/v5"
+	"time"
+)
+
+var jwtSecret = []byte("SECRET KEY")
+
+func GenerateAccessToken(userID, role string) (string, error) {
+	claims := jwt.MapClaims{
+		"role": role,
+		"exp":  time.Now().Add(time.Minute * 15).Unix(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString(jwtSecret)
+}
+
+func ValidationToken(tokenString string) (*jwt.Token, error) {
+	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method")
+		}
+		return jwtSecret, nil
+	})
+}
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	jwt "github.com/golang-jwt/jwt/v5"
+	"project/auth"
+)
+
+func main() {
+	token, jErr := jwti.GenerateAccessToken("23", "admin")
+	if jErr != nil {
+		fmt.Println("you have an error to generate token !")
+
+		return
+	}
+	fmt.Printf("token generated! %+v ", token)
+
+	parsedToken, vErr := jwti.ValidationToken(token)
+	if vErr != nil {
+		fmt.Printf("token is not valid :%+v", vErr)
+	}
+	if claims, ok := parsedToken.Claims.(jwt.MapClaims); ok && parsedToken.Valid {
+		fmt.Println("Token is valid")
+		fmt.Println("User ID:", claims["sub"])
+		fmt.Println("Role:", claims["role"])
+	} else {
+		fmt.Println("Token is invalid")
+	}
+}
